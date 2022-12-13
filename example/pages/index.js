1d96d83b8ebde029b859a9ee094f3941fced6d16
Leonard <leonard.lim@mediacliq.my>
2022-12-13 11:35:08 +0800

    Added basic pages

---
+++ example/pages/index.js
@@ -0,0 +1,14 @@
+import Styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.float_container}>
+        <div className={`${ Styles.float_child} ${ Styles.left_child}`}>
+          <p>WELCOME</p>
+          <p className={`${Styles.dot} ${Styles.bounce}`}>.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
