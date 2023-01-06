f5bde41126e97c721422f4ab96d8a54c3f434295
ragingSkull <boonfu87@gmail.com>
2023-01-06 12:14:46 +0800

    Fix styling

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='index-container'>
-        <div className={Styles.float_container}>
+    <div className='container !max-w-none'>
+        <div className='sub-container'>
             <div className={`${ Styles.float_child} ${ Styles.left_child}`}>
             <p>WELCOME</p>
             <p className={`${Styles.dot} ${Styles.bounce}`}>.</p>
