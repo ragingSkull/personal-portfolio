e5517cfab598c6a7ff36ff9bbfd3337b31c4d1af
ragingSkull <boonfu87@gmail.com>
2023-01-16 08:33:41 +0800

    Minor change

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
 import '../styles/app.css'
-import NaviBar from '../components/navi-bar';
-import Footer from '../components/footer';
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import Head from 'next/head'
+import NaviBar from '../components/navi-bar'
+import Footer from '../components/footer'
+import { DefaultSeo } from 'next-seo'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false 
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+    const url = `https://my-first-spa.vercel.app${router.route}`
+
     return ( 
-     <> 
+    <>
+    <Head>
+        <link rel="icon" href="/static/favicon.png"></link>
+        <title>Leonard</title>
+    </Head>
+    <DefaultSeo
+        titleTemplate="%s - Leonard"
+        openGraph={{
+            type: 'website',
+            locale: 'en_IE',
+            url,
+            description: 'The personal website of Leonard.',
+            site_name: 'Leonard',
+            images: [],
+        }}
+        canonical={url}
+    /> 
     <NaviBar />
     <Component {...pageProps } />
     <Footer />
