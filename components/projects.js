1d990ee3a947e5a78b7c03589612ecc23ea5c41d
Leonard <leonard.lim@mediacliq.my>
2023-08-29 11:43:42 +0800

    Revamp

---
+++ components/projects.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className="mt-10">
+            <h1 className="text-[#ffffff] text-3xl">Projects</h1>
+            <div className="grid grid-cols-3 gap-4 mt-4">
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a className="underline" href="https://github.com/ragingSkull/my-first-spa">rotten • egg</a>
+                    </h2>
+                    <p className="text-[#737373]">A simple personal portfolio</p>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a href="">coming soon</a>
+                    </h2>
+                    <p className="text-[#737373]">Stay tuned for more updates!</p>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a href="">coming soon</a>
+                    </h2>
+                    <p className="text-[#737373]">Stay tuned for more updates!</p>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a href="">coming soon</a>
+                    </h2>
+                    <p className="text-[#737373]">Stay tuned for more updates!</p>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a href="">coming soon</a>
+                    </h2>
+                    <p className="text-[#737373]">Stay tuned for more updates!</p>
+                </div>
+                <div className="p-4">
+                    <h2 className="text-[#ffffff] text-lg">
+                        <a href="">coming soon</a>
+                    </h2>
+                    <p className="text-[#737373]">Stay tuned for more updates!</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects
