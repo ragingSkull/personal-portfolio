d364a89757beda448cd8e60831a04a34eeefcc2a
ragingSkull <leonard.lim@mediacliq.my>
2023-12-14 14:38:49 +0800

    Remove duplicated social media tags

--- components/header.js
+++ components/header.js
@@ -6,10 +6,6 @@ function Header({page, description}) {
             <link rel="icon" href="/static/favicon.png" />
             <title>{page} | rotten • egg</title>
             <meta name="description" content={description} />
-            <meta property="og:url" content="https://rottenegg.dev" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="rotten • egg" />
-            <meta property="og:description" content={description} />
             <meta property="og:image" content="/static/favicon.png" />
         </Head>
     );
