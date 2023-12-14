ce1abc0ed11abf9dc064103b0ff839014781b26a
ragingSkull <leonard.lim@mediacliq.my>
2023-12-14 14:19:52 +0800

    Change index title styling

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import AboutMe from '../components/about-me'
 import Projects from '../components/projects'
 import Blog from '../components/blog'
 import Footer from '../components/footer'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='container max-w-2xl mx-auto'>
-        <p className='text-[#737373] text-4xl font-medium' style={{fontFamily: 'Kong',}}>rotten . egg</p>
+        <p className='flex flex-wrap text-[#FFFF00] text-4xl font-medium' style={{fontFamily: 'Kong', textShadow: '2px 2px #737373'}}>rotten <Image src='/static/favicon.png' alt='rotten . egg' width={100} height={50} className='px-4' /> egg</p>
         <p className='text-[#737373] text-base font-light mt-14'>WELCOME — to my corner of the internet ; / {'< 欢迎  >'}, {'{ Selamat Datang }'} ...</p>
         <AboutMe />
         <Projects />
